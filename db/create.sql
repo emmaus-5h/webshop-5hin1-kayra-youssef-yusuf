f3ac82ede9150b31e45a6a7e4fb5c43b457645fb
YusufAliskan <93194825+YusufAliskan@users.noreply.github.com>
2023-02-17 18:02:50 +0000

    alle producten heb ik afgemaakt nu alleen nog ze in de juiste menu krijgen!

--- db/create.sql
+++ db/create.sql
@@ -51,26 +51,42 @@ CREATE TABLE deliverytime (
 --
 
 
+--Rolex horloges
 
-insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Rolex GMT-Master', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 24600, 26000, 2, 2, 1, 1, 'Rolex');
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('GMT-Master', 'Dit is het nieuwste exemplaar, ook wel pepsi genoemd.', '816905633-0', 24600, 26000, 2, 1, 1, 1, 'rolex');
 
-insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Rolex Day-Date', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 16000, 18000, 2, 2, 1, 1, 'Rolex');
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Day-Date', 'Dit horloge is compleet van goud gemaakt en heeft een diamond-plate.', '077030122-3', 16000, 18000,  2, 1, 1, 1, 'rolex');
 
-insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category ) values ('Rolex Daytona', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 15000, 17250, 2, 2, 1, 1, 'Rolex');
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category ) values ('Daytona', 'Prachtige horloge gemaakt van staal en titanium. ', '693155505-7', 15000, 17250, 2, 2, 1, 1, 'rolex');
 
-insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Rolex Datejust', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14000, 16950, 2, 2, 1, 1, 'Rolex');
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Datejust', 'Dit is ook wel het meest bekende horloge van rolex, het is gemaakt van.', '686928463-6', 14000, 16950, 2, 2, 1, 1, 'rolex');
 
+--Audemars Piquet horloges
 
-insert into products (name, description, code, price) values ('Audemars piquet Royal Oak', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Royal Oak', 'Dit geweldige horloge is gemaakt van black ceramic.', '686928463-6', 42000, 63500, 2, 2, 1, 1, 'ap');
 
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Millenary', 'Dit is het eerste horloge dat Audemars piquet uitgebracht heeft.', '077030122-3', 21000, 27350, 2, 2, 1, 1, 'ap');
 
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Royal Oak Offshore', 'Dit horloge heeft een leren band en is van staal gemaakt.', '077030122-3', 51000, 53500, 2, 1, 1, 1, 'AP');
+
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Jules Audemars', 'Dit horloge is gemaakt van goud met een leren band.', '077030122-3', 34000, 49000, 2, 1, 1, 1, 'ap');
+
+--Patek Philippe horloges
+
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Nautilus', 'Dit is het perfecte horloge gemaakt van ceramic en.', '077030122-2', 55000,145000, 2, 1, 1, 1, 'patek');
+
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Aquanaut ', 'Beeldschone horloge gemaakt van stainless steel.', '077030122-2', 33000,99000, 2, 1, 1, 1, 'patek');
+
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Complications ', 'Prachtige horloge gemaakt van witte ceramic.', '077030122-2', 46500,79000, 2, 1, 1, 1, 'patek');
+
+insert into products (name, description, code, price, resell, stock_id, interest_id, supply_id, time_id, category) values ('Gondolo', 'Het oudste exemplaar van Patek Philippe gemaakt van goud en heeft een speciale leren band.', '077030122-2', 11000,17500, 2, 1, 1, 1, 'patek');
 
 --stocks
-insert into stocks (instock) values ('Almost sold out');
-insert into stocks (instock) values ('Enough in stock');
+insert into stocks (instock) values ('Bijna uitverkocht');
+insert into stocks (instock) values ('Instock');
 
 --interest
-insert into interest (name) values ('Popular');
+insert into interest (name) values ('Populair');
 insert into interest (name) values ('Underrated');
 
 --supplier
